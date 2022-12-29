47d57b1388dc83a735c1752f2b3f685ab4bbf816
IbrahimSaid1 <squ118977@gmail.com>
2022-12-29 16:54:57 +0400

    Update

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -1,52 +1,59 @@
 package Test;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.ArrayList;	
 import java.util.Scanner;
-import java.util.Stack;
 
 public class Main {
 
 	public static void main(String[] args) {
 		ArrayList<Student> stu = new ArrayList<Student>();
-		Stack<String> myStack = new Stack<>();
 		Scanner sca = new Scanner(System.in);
 
-//		Student obj = new Student();
-//		System.out.print("Please enter student name: ");
-//		String stusentName = sca.next();
-//		obj.setStudentName(stusentName);
-//		myStack.push(stusentName);
-//		System.out.print("Please enter the mark: ");
-//		int mark = sca.nextInt();
-//		obj.setMark(mark);
-//		String mar = Integer.toString(mark);
-//		myStack.push(mar);
-
-
-		
 			try {
-				for(int i=1; i<=5; i++) {
+				for(int i=1; i<=100; i++) {
 				BufferedWriter writer = new BufferedWriter(new FileWriter(i+".pdf"));
 				Student obj = new Student();
 				System.out.print("Please enter student name: ");
 				String stusentName = sca.next();
 				obj.setStudentName(stusentName);
-				//myStack.push(stusentName);
 				
 			
-				writer.write("Student File ");
-					 
+				writer.write("Student File... ");
 					writer.write("\n" + stusentName);
-				
 					writer.close();
 				}
-				
+
 			} catch (IOException a) {
 			System.out.println("Error");
 			a.printStackTrace();
 		}
+			
+			File folder= new File("pdf");
+			boolean option=true;
+			while(option) {
+				System.out.println("Do you want to search?\n1-Yes\n2-No");
+				int j = sca.nextInt();	
+				if(j==1) {
+				System.out.print("Input the name to find: ");
+				String name = sca.next();			 
+				if(name.contains(name)) {
+				     System.out.println("Found");
+				     folder.mkdir();
+				 }
+				 else {
+				     System.out.println("Not Found");
+				 }
+				}
+				else if(j==2) {
+					System.out.println("Thank You");
+					option=false;
+					
+				}
+			}
+			
 
 	}
 	
