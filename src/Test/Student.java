c46b7496f9dc511f1f0ea21e6a0e037c523cf993
IbrahimSaid1 <squ118977@gmail.com>
2022-12-29 12:35:19 +0400

    Time 1

---
+++ src/Test/Student.java
@@ -0,0 +1,24 @@
+package Test;
+import java.util.ArrayList;
+
+public class Student {
+	
+	private String studentName;
+	private int mark;
+	
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public int getMark() {
+		return mark;
+	}
+	public void setMark(int mark) {
+		this.mark = mark;
+	}
+	
+	
+
+}
